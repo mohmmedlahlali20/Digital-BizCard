9d02fcfa372bc7b5df4d2ec43c89b409101ccfff
MohammedLahlali <mohmmedlaeh81@gmail.com>
2024-03-25 14:03:16 +0000

    commit 1

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //REgister API 
+     public function register(Request $request){
+
+        //data validation
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+//save data
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            "status" => true,
+            "message" => 'register Successfuly'
+        ]);
+
+     }
+
+     //login API
+
+     public function login(Request $request){
+
+        $request->validate([
+                    'email' => 'required',
+                    'password' => 'required',
+                ]);
+
+                $user = User::where('email' ,  $request->email )->first();
+                if(!empty($user)){
+                    if(Hash::check($request->password , $user->password)){
+                              $token = $user->createToken("MyToken")->plainTextToken;
+
+                       return response()->json([
+                                "status" => true ,
+                                "message" => "login successfuly",
+                                'token' =>   $token
+                        ]);
+                    }
+                }
+
+                return response()->json([
+                    "status" => false ,
+                    "message" => "invalid data user dosn't exist"
+        ]);
+     }
+
+     //Profil API 
+
+     public  function profile(){
+        $data = auth()->user();
+
+        return response()->json([
+            "status" => true,
+            "message" => "Your profile hhhh",
+            "user"=> $data,
+        ]);
+
+     }
+
+     //logout API 
+     public function logout(){ 
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            "status" => true,
+            "message" => "user logged successufly"
+        ]);
+
+     }
+
+
+
+}
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+  //  return $request->user();
+//});
+
+
+//API route
+
+
+Route::post('register' , [UserController::class , 'register']);
+Route::post('login' , [UserController::class , 'login']);
+ROute::group([
+    "middleware" => ["auth:sanctum"]
+
+] , function(){
+
+    Route::get('profile' , [UserController::class , 'profile']);
+    Route::get('logout' , [UserController::class , 'logout']);
+
+});
